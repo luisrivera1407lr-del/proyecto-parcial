19f7ce3c616570074ba45b190ccb89332db13aba
luisrivera1407lr-del <luisrivera1407.lr@gmail.com>
2026-05-03 17:53:45 -0500

    Update query.sql

--- scripts/query.sql
+++ scripts/query.sql
@@ -5,33 +5,6 @@ IF OBJECT_ID('dbo.sp_IncidenciasPorComercio', 'P') IS NOT NULL
     DROP PROCEDURE dbo.sp_IncidenciasPorComercio;
 GO
 
-CREATE PROCEDURE dbo.sp_IncidenciasPorComercio
-    @id_comercio INT
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    SELECT
-        I.id_incidencia,
-        C.nombre_comercial,
-        T.nombre AS Tipo_Incidencia,
-        P.nombre AS Prioridad,
-        I.impacto_financiero,
-        I.fecha_creacion
-    FROM INCIDENCIA I
-    INNER JOIN COMERCIO C
-        ON I.id_comercio = C.id_comercio
-    INNER JOIN TIPOINCIDENCIA T
-        ON I.id_tipo = T.id_tipo
-    INNER JOIN PRIORIDAD P
-        ON I.id_prioridad = P.id_prioridad
-    WHERE I.id_comercio = @id_comercio
-    ORDER BY I.fecha_creacion DESC;
-END;
-GO
-
-EXEC dbo.sp_IncidenciasPorComercio @id_comercio = 1
-
 CREATE PROCEDURE sp_LineaXtiempoXincidencia
     @id_incidencia INT
 AS
